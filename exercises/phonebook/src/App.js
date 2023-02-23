5d2062ae515c5c809d404f8e5d4394ab0bfb6f0c
Casper White <sahniabhi11@gmail.com>
2023-02-23 01:27:31 -0800

    Completed 2.d :D

--- exercises/phonebook/src/App.js
+++ exercises/phonebook/src/App.js
@@ -1,24 +1,23 @@
 import { useState, useEffect } from 'react'
 import SearchName from './components/SearchName'
 import DisplayPersons from './components/DisplayPerson'
-import axios from 'axios'
+
+import phoneService from './services/phone'
 
 
 
 const App = () => {
   const [persons, setPersons] = useState([])
-const [newName, setNewName] = useState('')
-const [newNumber, setNewNumber] = useState('')
-const [nameSearch, setNameSearch] = useState('')
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [nameSearch, setNameSearch] = useState('')
 
 
 const hook = () => {
-  console.log('effect')
-  axios
-    .get('http://localhost:3001/persons')
-    .then(response => {
-      console.log('promise fulfilled')
-      setPersons(response.data)
+  phoneService
+    .getAll()
+    .then(initialNotes => {
+      setPersons(initialNotes)
     })
 }
 
@@ -30,18 +29,50 @@ const handleFormSubmit = (event) => {
   const nameExists = persons.find(person => person.name === newName)
   if(nameExists)
   {
-    alert(`${newName} is already added to phonebook.`)
+    if(window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`))
+    {
+      console.log(`Name Exists id: ${nameExists.id}`)
+      const newPersonObject = {
+        name: newName,
+        number: newNumber,
+      }
+      phoneService
+        .update(nameExists.id, newPersonObject)
+        .then(returnedNote => {
+          setPersons(persons.map(person => person.id !== nameExists.id ? person: returnedNote))
+        })
+        setNewName('')
+        setNewNumber('')
+
+      
+    }
   }
   else{
     const newPersonObject = {
       name: newName,
       number: newNumber,
     }
-    setPersons(persons.concat(newPersonObject))
-    setNewName('')
-    setNewNumber('')
+
+    phoneService
+      .create(newPersonObject)
+      .then(returnedNote => {
+        setPersons(persons.concat(returnedNote))
+        setNewName('')
+        setNewNumber('')
+      })
   }
 
+}
+
+
+const deleteEntry = (person) => {
+  console.log(`Delete button for id: ${person.id} clicked`)
+  if(window.confirm(`Delete ${person.name} ?`))
+  { 
+    phoneService.remove(person.id)
+    const newPersons= persons.filter(people => people.id !== person.id)
+    setPersons(newPersons)
+  }
 }
 
 const handleNameChange = (event) => {
@@ -55,6 +86,8 @@ const handleNumberChange = (event) => {
 const handleFitler = (event) => {
   setNameSearch(event.target.value)
 }
+
+
 
   return (
     <div>
@@ -71,22 +104,11 @@ const handleFitler = (event) => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.filter(person => person.name.match(nameSearch)).map((person, index) => <DisplayPersons person={person.name} number={person.number} key={index}/>)}
+      
+      {persons.filter(person => person.name.match(nameSearch)).map((person, index) => <DisplayPersons person={person} key={index} deleteEntry={deleteEntry} />)}
       
     </div>
   );
 }
 
 export default App;
-
-
-/*
-
-const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
-*/
--- exercises/phonebook/src/components/DisplayPerson.js
+++ exercises/phonebook/src/components/DisplayPerson.js
@@ -1,6 +1,24 @@
-const DisplayPersons = ({person, number}) => {
+import phoneService from '../services/phone'
+
+const DisplayPersons = ({person, deleteEntry}) => {
+
+  // const handleDelete = () => {
+  //   console.log(`Delete button for id: ${personId} clicked`)
+  //   if(window.confirm(`Delete ${person} ?`))
+  //   { 
+  //     phoneService.remove(personId)
+  //     //.then(returnedPB => console.log(returnedPB))
+  //     persons.filter(person => persons.id !== personId)
+  //     setPersons(persons)
+  //   }
+  // }
+
+  const handleDelete = () => {
+    deleteEntry(person)
+  }
+
     return(
-      <div>{person} {number}</div>
+      <div>{person.name} {person.number} <button onClick={handleDelete}>delete</button></div>
     )
   }
 
