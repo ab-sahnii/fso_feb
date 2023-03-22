5c7977af1d88267ba1e4f2d98f973a994c81a777
Casper White <sahniabhi11@gmail.com>
2023-03-21 22:41:21 -0700

    Deploying app to internet

--- part3/part3a/index.js
+++ part3/part3a/index.js
@@ -1,13 +1,12 @@
 const express = require('express')
-const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
 
-const requestLogger = (request, response, next) => {
-    console.log('Method: ', request.method)
-    console.log('Path: ', request.path)
-    console.log('Body: ', request.body)
-    console.log('---')
-    next()
-}
+const app = express()
+app.use(express.static('build'))
+app.use(cors())
+app.use(morgan('combined'))
+
 
 const unknownEndpoint = (request, response)=> {
     response.status(404).send({error: 'unknown endpoint'})
@@ -15,7 +14,6 @@ const unknownEndpoint = (request, response)=> {
 
 
 app.use(express.json())
-app.use(requestLogger)
 
 
 let notes = [
@@ -97,7 +95,7 @@ app.use(unknownEndpoint)
 
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
